701cff0700beb275f6a6f50261898b7e0b3cc8b1
Brandon Ramirez <143334896+bramirez003@users.noreply.github.com>
2023-11-05 23:53:34 -0500

    Add files via upload

---
+++ Chapter 5.1 - Finding The Lenght of a String.sql
@@ -0,0 +1,1 @@
+SELECT name, LENGTH(Name) AS Len FROM City ORDER BY Len DESC, Name;
---
+++ Chapter 5.3 - Removing Spaces.sql
@@ -0,0 +1,4 @@
+SELECT TRIM ('   string   ');
+SELECT LTRIM ('   string   ');
+SELECT RTRIM ('   string   ');
+SELECT TRIM ('...string...', '.');
