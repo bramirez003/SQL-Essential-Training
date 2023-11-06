016aee3cab584333e6b828d3a1315a440a98c990
Brandon Ramirez <143334896+bramirez003@users.noreply.github.com>
2023-11-05 22:50:53 -0500

    Add files via upload

---
+++ Chapter 4.2 - Relating Multiple Tables.sql
@@ -0,0 +1,16 @@
+INSERT INTO customer (name) VALUES ('Jane Smith');
+SELECT * FROM customer;
+
+SELECT c.name AS Cust, c.zip, i.name AS Item, i.description, s.quantity AS Quan, s.price AS Price
+  FROM sale AS s
+  JOIN item AS i ON s.item_id = i.id
+  JOIN customer AS c ON s.customer_id = c.id
+  ORDER BY Cust, Item
+;
+
+SELECT c.name AS Cust, c.zip, i.name AS Item, i.description, s.quantity AS Quan, s.price AS Price
+  FROM customer AS c
+  LEFT JOIN sale AS s ON s.customer_id = c.id
+  LEFT JOIN item AS i ON s.item_id = i.id
+  ORDER BY Cust, Item
+;
